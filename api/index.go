87cb92fa098d61911f567364e9517ac0c478305c
Trevor Atlas <me@trevoratlas.com>
2020-03-14 20:36:39 -0400

    maybe this

--- api/index.go
+++ api/index.go
@@ -2,18 +2,48 @@ package handler
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/trevor-atlas/weekend/api/users"
+	"github.com/trevor-atlas/weekend/api/utils"
 	"log"
 	"net/http"
-	u "github.com/trevor-atlas/weekend/api/utils"
 )
 
 // Handler is the function that Now calls for every request
 func Handler(w http.ResponseWriter, r *http.Request) {
-	defer u.Track(u.Runtime("handler"))
+	defer utils.Track(utils.Runtime("handler"))
 	log.Println("Request url: ", r.URL.Path)
 
-	name := u.GetParamWithDefault("name", "Guest", r)
+	switch r.URL.Path {
+	case "api/greet":
+		greet(w)
+	case "api/users":
+		switch r.Method {
+		case http.MethodGet:
+			getUser(w)
+		case http.MethodPost:
+			users.CreateUser(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "I can't do that.")
+		}
+	}
+
+}
+
+func greet(w http.ResponseWriter) {
+	name := utils.GetParamWithDefault("name", "Guest", r)
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, `<center><h1>Hello, %s from Go on Now!</h1></center>`, name)
+
+}
+
+func getUser(w http.ResponseWriter) {
+	stub := &users.User{
+		ID: uuid.New().String(),
+		Name: "Alex Jones",
+		Token: "lol yeah right",
+	}
+	utils.Write(stub, w)
 }
