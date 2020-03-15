9f6ae7a24bbcb01053dfd8825f3ac425d7f7636b
Trevor Atlas <me@trevoratlas.com>
2020-03-15 00:33:21 -0400

    logging and extras

--- api/router/router.go
+++ api/router/router.go
@@ -4,24 +4,30 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"strconv"
 )
 
 type StupidRouter struct {
 	base string
 	routes map[string]http.HandlerFunc
 	errors []error
+	debug bool
 }
 
 func NewStupidRouter(basePath string) *StupidRouter {
-	return &StupidRouter{
-		base: basePath,
+	return &StupidRouter {
+		base: trimLeadingSlash(basePath),
 		routes: make(map[string]http.HandlerFunc),
 		errors: make([]error, 0),
+		// set this to false
+		debug: getEnvAsBool("DEBUG", true),
 	}
 }
 
 func (sr *StupidRouter) register(method string, route string, handler http.HandlerFunc) *StupidRouter {
-	key := trimLeadingSlash(method + "-" + sr.base + route)
+	key := hashPath(method, sr.base, route)
 	if _, ok := sr.routes[key]; ok {
 		err := errors.New("More than one route matched <" + route + "> for method <" + method + ">")
 		sr.errors = append(sr.errors, err)
@@ -62,21 +68,28 @@ func (sr *StupidRouter) Start(w http.ResponseWriter, r *http.Request) {
 		for _, err := range sr.errors {
 			message += err.Error() + ",\n"
 		}
+		logMsg("Error building router:\n%s", message)
 		http.Error(w, message, 500)
 	}
-	key := trimLeadingSlash(r.Method + "-" + r.URL.Path)
-	log.Println("matched key: ", key)
+	key := hashPath(r.Method, "", r.URL.Path)
 	if cb, ok := sr.routes[key]; ok {
+		logMsg("matched key: ", key)
 		cb(w, r)
 		return
 	}
 	if fallback, ok := sr.routes["DEFAULT"]; ok {
+		logMsg("Use DEFAULT handler")
 		fallback(w, r)
 		return
 	} else {
+		logMsg("Use generic 404 handler")
 		http.Error(w, "Not found", 404)
 		return
 	}
+}
+
+func hashPath(method, base, route string) string {
+	return path.Join(method + ": ", base, route)
 }
 
 func trimLeadingSlash(str string) string {
@@ -84,4 +97,26 @@ func trimLeadingSlash(str string) string {
 		str = str[:len(str)-1]
 	}
 	return str
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+func getEnvAsBool(name string, defaultVal bool) bool {
+	valStr := getEnv(name, "")
+	if val, err := strconv.ParseBool(valStr); err == nil {
+		return val
+	}
+	return defaultVal
+}
+
+func logMsg(m ...interface{}) {
+	if ok := getEnvAsBool("DEBUG", true); ok {
+		log.Println(m)
+	}
 }
